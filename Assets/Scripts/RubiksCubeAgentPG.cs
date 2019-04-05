a0942e50ef6676a478194d72e21035de673e06c8
Caio SOUZA <caioc2bolado@gmail.com>
2019-04-05 18:38:53 -0300

    added missing scene

---
+++ Assets/Scripts/RubiksCubeAgentPG.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MLAgents;
+
+public class RubiksCubeAgentPG : Agent
+{
+
+    [Header("Specific to Rubiks Cube")]
+    private RubiksCubePrefab rcp;
+    public override void InitializeAgent()
+    {
+        rcp = transform.gameObject.GetComponent<RubiksCubePrefab>() as RubiksCubePrefab;
+
+        agentParameters.onDemandDecision = true;
+        agentParameters.resetOnDone = false;
+        agentParameters.maxStep = 0;
+    }
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(rcp.getCubeState());
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        if (vectorAction.Length != 1)
+        {
+            Debug.LogError("Action vector length != than 1");
+        }
+        else
+        {
+            RubiksCube.move m = (RubiksCube.move)vectorAction[0];
+            rcp.runAnimatedMove(m);
+        }
+    }
+
+    private bool toSolve = false;
+    public void Solve()
+    {
+        toSolve = true;
+    }
+
+    private bool waitRequest = false;
+    public void FixedUpdate()
+    {
+        if (toSolve)
+        {
+            if (!waitRequest)
+            {
+                RequestDecision();
+                waitRequest = true;
+            }
+            else if (!rcp.isAnimationInProgress() && rcp.movesToAnim() == 0)
+            {
+                waitRequest = false;
+                if (rcp.isSolved())
+                {
+                    toSolve = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneController : MonoBehaviour {
+
+    public GameObject rubiksCube;
+    private RubiksCubePrefab rcp;
+    private RubiksCubeAgentPG rca;
+    // Use this for initialization
+    void Awake () {
+        rcp = rubiksCube.GetComponent<RubiksCubePrefab>() as RubiksCubePrefab;
+        rca = rubiksCube.GetComponent<RubiksCubeAgentPG>() as RubiksCubeAgentPG;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void L()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.LEFT);
+    }
+
+    public void Lcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.LEFTCC);
+    }
+    public void R()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.RIGHT);
+    }
+
+    public void Rcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.RIGHTCC);
+    }
+    public void U()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.TOP);
+    }
+
+    public void Ucc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.TOPCC);
+    }
+    public void D()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.BOTTOM);
+    }
+
+    public void Dcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.BOTTOMCC);
+    }
+    public void B()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.BACK);
+    }
+
+    public void Bcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.BACKCC);
+    }
+    public void F()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.FRONT);
+    }
+
+    public void Fcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.FRONTCC);
+    }
+
+    public void X()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.X);
+    }
+
+    public void Xcc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.XCC);
+    }
+
+    public void Y()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.Y);
+    }
+
+    public void Ycc()
+    {
+        rcp.runAnimatedMove(RubiksCube.move.YCC);
+    }
+
+    public void reset()
+    {
+        rcp.resetCube();
+    }
+
+
+    public void scramble1()
+    {
+        rcp.scramble(1);
+    }
+
+    public void scramble10()
+    {
+        rcp.scramble(10);
+    }
+
+    public void scramble100()
+    {
+        rcp.scramble(100);
+    }
+
+    public void solve()
+    {
+        rca.Solve();
+    }
+}
